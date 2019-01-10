7bc5905f259e7cc9f96d692b518abe95dd11924f
450128264@qq.com <450128264@qq.com>
2019-01-10 18:08:49 +0800

    just up

--- src/Dao/Redis/RedisHelper.php
+++ src/Dao/Redis/RedisHelper.php
@@ -9,9 +9,33 @@
 namespace Qyk\Mm\Dao\Redis;
 
 
+use Qyk\Mm\Facade\AbstractConnectService;
 use Qyk\Mm\Traits\SingletonTrait;
 
-class RedisHelper
+/**
+ * redis服务
+ * Class RedisHelper
+ * @package Qyk\Mm\Dao\Redis
+ */
+class RedisHelper extends AbstractConnectService
 {
     use SingletonTrait;
+
+    /**
+     * 建立连接
+     * @param  $params
+     * @return mixed
+     */
+    protected function buildConnect($params = null)
+    {
+        // TODO: Implement buildConnect() method.
+    }
+
+    /**
+     * 关闭操作
+     */
+    protected function distConnect()
+    {
+        // TODO: Implement distConnect() method.
+    }
 }
